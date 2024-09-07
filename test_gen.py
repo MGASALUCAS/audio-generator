53aa509bc0993b3ca96d6e61d8f673b7027a44c2
Computer Wizard <mgasa.loucat1@gmail.com>
2024-09-07 21:54:09 +0300

    the initial commit

--- test_gen.py
+++ test_gen.py
@@ -2,6 +2,6 @@ from elevenlabs import VoiceSettings
 
 from elevenlabs import set_api_key, generate
 
-set_api_key("d53c9dd6499c7fdca63c14f4348b779c")
+set_api_key("gyiu")
 
 audio_data = generate(text="Hello world!", voice="Adam")
